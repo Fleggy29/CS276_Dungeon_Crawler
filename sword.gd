1b230d4a7c03be13b107dfa562b0570f82da8ed0
Alex Flegg <u5601678@live.warwick.ac.uk>
2025-10-17 11:45:23 +0100

    first commit

--- sword.gd
+++ sword.gd
@@ -6,7 +6,7 @@ func _ready() -> void:
 	#hide()
 	pivot = get_parent()
 	
-#func _process(delta: float) -> void:
-	#pivot.rotation = Vector2(Global.player.position.x, -1).angle_to(get_global_mouse_position()) - PI/2
-	#print("position", Vector2(Global.player.position.x, -1))
-	#print("angle", Vector2(Global.player.position.x, -1).angle_to(get_global_mouse_position()) - PI/2)
+func _process(delta: float) -> void:
+	pivot.rotation = Vector2(Global.player.position.x, -1).angle_to(get_global_mouse_position()) - PI/2
+	print("position", Vector2(Global.player.position.x, -1))
+	print("angle", Vector2(Global.player.position.x, -1).angle_to(get_global_mouse_position()) - PI/2)

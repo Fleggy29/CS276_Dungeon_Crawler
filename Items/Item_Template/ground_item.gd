9ab90a178df51f8304383447c2dbd3ceb3cb2b1c
Alexander Golovatyy <94749662+T0mOrJerry@users.noreply.github.com>
2025-12-12 04:10:48 +0000

    Item generation & boat

--- Items/Item_Template/ground_item.gd
+++ Items/Item_Template/ground_item.gd
@@ -1,6 +1,7 @@
 class_name GroundItem extends Area2D
 signal ground_item_body_entered(body:Node2D, emitter:Node2D)
 var highlightCol = Color(255,255,255,0)
+#signal _on_body_entered
 
 #func _init(player:Player) -> void:
 	#self.ground_item_body_entered.connect(player._on_ground_item_body_entered)
